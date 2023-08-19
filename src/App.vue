296f671d44b6430e933da7ecff175484787a4444
zy666946 <75345490+zy666946@users.noreply.github.com>
2023-08-19 10:55:41 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import socket from './components/socket.vue'
+</script>
+
+<template>
+        <socket></socket>
+</template>
+
+<style>
+        *{
+                font-size: 12px;
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+
+        }
+</style>
