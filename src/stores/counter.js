a63a58d651fad725e583e9520333a2310fbd694b
zy666946 <75345490+zy666946@users.noreply.github.com>
2023-09-06 11:05:51 +0800

    Update counter.js

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,14 +1,52 @@
 import { defineStore } from "pinia";
+import { io } from 'socket.io-client'
 
 export const useMessage = defineStore('message', {
         state: () => ({
-                datas: []
+                datas: {
+		},
+		io: io,
+		onLineUserList: [],
+		
 
         }),
         actions: {
                 addMessage(newMessage){
-                        this.datas.push(newMessage)
-                }
-        }
+			if(newMessage.name === 'my'){
+				
+                        	this.datas[newMessage.to]?this.datas[newMessage.to].push(newMessage):this.datas[newMessage.to] = [newMessage]
+			
+			}else{
+				this.datas[newMessage.name]?this.datas[newMessage.name].push(newMessage):this.datas[newMessage.name] = [newMessage]
+			}
+                },
+		addOffMessage(offMessage){
+			//this.datas.push(...offMessage)
+			
+			console.log(Object.keys(offMessage),offMessage)
+			Object.keys(offMessage).forEach((item)=>{
+				this.datas[item]?this.datas[item].push(...offMessage[item]):this.datas[item] = [...offMessage[item]]
+			})
+			
+		},
+		upDateOnLineUserList(newUserList){
+			console.log(newUserList)
+			this.onLineUserList = newUserList
+		}
+        },
+	getters: {
+		
+		userList: (state)=>{
+			let setList = new Set()
+			Object.keys(state.datas).forEach((item)=>{
+				setList.add(item)
+			})
+			state.onLineUserList.forEach((item)=>{
+				setList.add(item)
+			})
+			return setList
+		}
+		
+	}
 
 })
