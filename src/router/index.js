6deea6e0e162a40eaff512cd6de27695591dc72c
zy666946 <75345490+zy666946@users.noreply.github.com>
2023-09-06 11:05:35 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import socket from '../components/socket.vue'
+import talk from '../components/talk.vue'
+import home from '../components/Home.vue'
+import login from '../components/Login.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {
-      path: '/',
-      name: 'socket',
-      component: socket
-    }
-  ]
-})
+	{
+      		path: '/',
+		redirect: {
+			path: '/home/test'
+		}
+    	},
+	{
+		path: '/login',
+		name: 'login',
+		component: login
+	},
+	{
+		path: '/home',
+		redirect: {
+			path: '/home/test'
+		}
+	},
+	{
+		path: '/home/:name',
+		name: 'home',
+		component: home
+		  
+	},
+	{
+		path: '/talk/:name/:toname',
+		name: 'talk',
+		component: talk,
+		props: true
+	}
+	]
+    })
 
 export default router
